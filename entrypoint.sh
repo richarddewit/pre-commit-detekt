c1e4d7533ddbe6263f0b00a09e14e6372af46246
quwac <53551867+quwac@users.noreply.github.com>
2022-12-27 02:49:37 +0900

    Support base-path

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,13 @@ opts=()
 filenames=""
 filenames_started=0
 input_included=0
+base_path_included=0
 for i in "$@"; do
     if [ "$i" == "--input" ] || [ "$i" == "-i" ]; then
         input_included=1
+    fi
+    if [ "$i" == "--base-path" ] || [ "$i" == "-bp" ]; then
+        base_path_included=1
     fi
 
     if [ $filenames_started -eq 0 ]; then
@@ -28,9 +32,15 @@ for i in "$@"; do
     fi
 done
 
+if [[ $base_path_included -eq 0 ]]; then
+    opts+=("--base-path" "/src")
+fi
+
 # run detekt
+pushd /src
 if [ "$filenames" == "" ] || [[ $input_included -eq 1 ]]; then
     java -jar "/opt/detekt/detekt-cli-all.jar" "${opts[@]}"
 else
     java -jar "/opt/detekt/detekt-cli-all.jar" "${opts[@]}" --input "$filenames"
 fi
+popd
