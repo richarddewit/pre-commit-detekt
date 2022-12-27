147676aaefef91ee02954862ac6d380b81e185d7
quwac <53551867+quwac@users.noreply.github.com>
2022-12-27 11:35:51 +0900

    Convert bash to sh script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,37 @@
-#!/bin/bash
+#!/bin/sh
 
 set -u
 
+ends_with() {
+    target="${1}"
+    suffix="${2}"
+
+    case $target in
+    *$suffix)
+        echo 0
+        ;;
+    *)
+        echo 1
+        ;;
+    esac
+}
+
 # parse arguments
-opts=()
+opts=""
 filenames=""
 filenames_started=0
 input_included=0
 base_path_included=0
 for i in "$@"; do
-    if [ "$i" == "--input" ] || [ "$i" == "-i" ]; then
+    if [ "$i" = "--input" ] || [ "$i" = "-i" ]; then
         input_included=1
     fi
-    if [ "$i" == "--base-path" ] || [ "$i" == "-bp" ]; then
+    if [ "$i" = "--base-path" ] || [ "$i" = "-bp" ]; then
         base_path_included=1
     fi
 
     if [ $filenames_started -eq 0 ]; then
-        if [[ ${i} == *.kt ]] || [[ ${i} == *.kts ]]; then
+        if [ "$(ends_with "$i" ".kt")" -eq 0 ] || [ "$(ends_with "$i" ".kts")" -eq 0 ]; then
             filenames_started=1
         fi
     fi
@@ -28,20 +42,29 @@ for i in "$@"; do
         fi
         filenames="${filenames}${i}"
     else
-        opts+=("$i")
+        if [ "$opts" != "" ]; then
+            opts="$opts "
+        fi
+        opts="$opts$i"
     fi
 done
 
-if [[ $base_path_included -eq 0 ]]; then
-    opts+=("--base-path" "/src")
+if [ $base_path_included -eq 0 ]; then
+    if [ "$opts" != "" ]; then
+        opts="$opts "
+    fi
+    opts="${opts}--base-path /src"
 fi
 
 # run detekt
-pushd /src >/dev/null || exit 1
-if [ "$filenames" == "" ] || [[ $input_included -eq 1 ]]; then
-    OUTPUT=$(java -jar "/opt/detekt/detekt-cli-all.jar" "${opts[@]}" 2>&1)
+current_dir=$(pwd)
+cd /src >/dev/null || exit 1
+if [ "$filenames" = "" ] || [ $input_included -eq 1 ]; then
+    # shellcheck disable=SC2086
+    OUTPUT=$(java -jar "/opt/detekt/detekt-cli-all.jar" $opts 2>&1)
 else
-    OUTPUT=$(java -jar "/opt/detekt/detekt-cli-all.jar" "${opts[@]}" --input "$filenames" 2>&1)
+    # shellcheck disable=SC2086
+    OUTPUT=$(java -jar "/opt/detekt/detekt-cli-all.jar" $opts --input "$filenames" 2>&1)
 fi
 
 EXIT_CODE=$?
@@ -53,4 +76,4 @@ if [ $EXIT_CODE -ne 0 ]; then
     echo "***********************************************"
     exit $EXIT_CODE
 fi
-popd >/dev/null || exit 1
+cd "$current_dir" >/dev/null || exit 1
